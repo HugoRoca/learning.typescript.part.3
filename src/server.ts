88d1ea1a6d038401524182cef08974b98330f0e6
hugoroca <hugo.rock20@hotmail.com>
2020-02-12 22:47:13 -0500

    add process.on uncaughtException and unhandledRejection

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,20 @@ import { applyMiddleware, applyRoutes } from "./utils";
 import routes from "./services";
 import middleware from "./middleware";
 
+// el manejo tanto de uncaughtExeption y unhandledRejection es super importante, en caso
+// que nos encontremos con cualquier de estos dos, se acabó el juego para esta instancia
+// esto quiere decir que tu aplicación se quedara en un estado indefinido, asi que lo 
+// mejor es matar el proceso.
+process.on("uncaughtException", e => {
+    console.log(e);
+    process.exit(1)
+});
+
+process.on("unhandledRejection", e => {
+    console.log(e);
+    process.exit(1)
+});
+
 const router = express();
 applyMiddleware(middleware, router);
 applyRoutes(routes, router);
